39f89b891fe9979caa610f4ed1d1d851ad3005e1
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-24 08:54:05 -0400

    Second commit

---
+++ src/test/java/com/example/mathieufordexam/MathieuFordExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mathieufordexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathieuFordExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
